412f7fc41b494c69e000d5d6a354d206a07ddb6e
linskay <168856236+linskay@users.noreply.github.com>
2025-04-12 13:41:35 +0300

    Time: 5 ms (36.07%) | Memory: 41.6 MB (17.35%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -1,14 +1,20 @@
 public class Solution {
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
+        
+        // Создаем двумерный массив DP
         int[][] dp = new int[n][n];
         
-        for (int length = 2; length < n; ++length) {
+        // Заполняем таблицу DP по диагоналям
+        for (int length = 2; length < n; ++length) { // length — разница между i и j
             for (int i = 0; i < n - length; ++i) {
                 int j = i + length;
                 dp[i][j] = Integer.MAX_VALUE;
+                
+                // Перебираем все возможные треугольники с вершинами i, k, j
                 for (int k = i + 1; k < j; ++k) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
+                    dp[i][j] = Math.min(dp[i][j], 
+                                        dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
                 }
             }
         }
