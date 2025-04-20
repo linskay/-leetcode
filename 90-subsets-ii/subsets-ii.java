809a10bb4a0674a31c42a6d33dffeecd02e56e31
linskay <168856236+linskay@users.noreply.github.com>
2025-04-20 21:37:59 +0300

    Time: 5 ms (15.65%) | Memory: 44.6 MB (5.50%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList();
+        int n = nums.length;
+        Arrays.sort(nums);
+        int maxNumberOfSubsets = (int) Math.pow(2, n);
+        Set<String> seen = new HashSet<>();
+
+        for (int subsetIndex = 0; subsetIndex < maxNumberOfSubsets; subsetIndex++) {
+            List<Integer> currentSubset = new ArrayList();
+            StringBuilder hashcode = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                int mask = 1 << j;
+                int isSet = mask & subsetIndex;
+                if (isSet != 0) {
+                    currentSubset.add(nums[j]);
+                    hashcode.append(nums[j]).append(",");
+                }
+            }
+            if (!seen.contains(hashcode.toString())) {
+                seen.add(hashcode.toString());
+                subsets.add(currentSubset);
+            }
+        }
+
+        return subsets;
+    }
+}
