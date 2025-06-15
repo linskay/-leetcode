ae32a3590e673478e42247b1482d4a4a32997f9c
Alina <168856236+linskay@users.noreply.github.com>
2025-06-15 15:28:10 +0300

    Time: 2 ms (94.49%) | Memory: 46.7 MB (49.95%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int x = 1; x <= num; ++x) {
+            ans[x] = ans[x & (x - 1)] + 1;
+        }
+        return ans;
+    }
+}
