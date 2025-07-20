8c1b654023bef395dfbc4bf73ff63507942a3a7b
Alina <168856236+linskay@users.noreply.github.com>
2025-07-20 22:35:14 +0300

    Time: 0 ms (100.00%) | Memory: 41.1 MB (45.95%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,24 @@
+class Solution {
+    private int[] memo;
+
+    public int rob(int[] nums) {
+        this.memo = new int[100];
+        Arrays.fill(this.memo, -1);
+        return this.robFrom(0, nums);
+    }
+
+    private int robFrom(int i, int[] nums) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (this.memo[i] > -1) {
+            return this.memo[i];
+        }
+        int ans = Math.max(
+            this.robFrom(i + 1, nums),
+            this.robFrom(i + 2, nums) + nums[i]
+        );
+        this.memo[i] = ans;
+        return ans;
+    }
+}
