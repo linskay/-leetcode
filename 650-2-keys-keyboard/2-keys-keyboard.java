d07702bb72f86f526df151ee1242c2a264fbe824
Alina <168856236+linskay@users.noreply.github.com>
2025-08-06 21:21:42 +0300

    Time: 28 ms (20.45%) | Memory: 41.2 MB (31.96%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    dp[i] = Math.min(dp[i], dp[j] + i / j);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
