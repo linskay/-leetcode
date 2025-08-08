56d3b598bda01b3e03038704d8834d51128081b2
Alina <168856236+linskay@users.noreply.github.com>
2025-08-08 18:51:27 +0300

    Time: 1 ms (99.69%) | Memory: 46 MB (30.30%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        int diff = xor & -xor;
+        int[] res = new int[2];
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                res[0] ^= num;
+            } else {
+                res[1] ^= num;
+            }
+        }
+        return res;
+    }
+}
