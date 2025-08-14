d2e5f5cbe06e6e580430c255e7d4c28b2cb48c55
Alina <168856236+linskay@users.noreply.github.com>
2025-08-14 20:51:45 +0300

    Time: 25 ms (68.09%) | Memory: 42.7 MB (73.54%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        HashMap<Node, Node> visited = new HashMap();
+        LinkedList<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        visited.put(node, new Node(node.val, new ArrayList()));
+
+        while (!queue.isEmpty()) {
+            Node n = queue.remove();
+            for (Node neighbor : n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+                    queue.add(neighbor);
+                }
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+}
