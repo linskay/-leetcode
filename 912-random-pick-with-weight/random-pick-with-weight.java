cae3b831ffff57f7c8e4c6e83d7720a61070b079
Alina <168856236+linskay@users.noreply.github.com>
2025-08-14 20:49:36 +0300

    Time: 93 ms (8.44%) | Memory: 50 MB (47.25%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int[] prefixSums;
+    private int totalSum;
+
+    public Solution(int[] w) {
+        this.prefixSums = new int[w.length];
+
+        int prefixSum = 0;
+        for (int i = 0; i < w.length; ++i) {
+            prefixSum += w[i];
+            this.prefixSums[i] = prefixSum;
+        }
+        this.totalSum = prefixSum;
+    }
+
+    public int pickIndex() {
+        double target = this.totalSum * Math.random();
+        int i = 0;
+        for (; i < this.prefixSums.length; ++i) {
+            if (target < this.prefixSums[i])
+                return i;
+        }
+        return i - 1;
+  }
+}
