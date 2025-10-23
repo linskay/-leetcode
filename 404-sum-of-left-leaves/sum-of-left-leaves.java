45172b925d0c18fd9663bbe2a6d1ec73d68a5420
Alina <168856236+linskay@users.noreply.github.com>
2025-10-23 20:57:17 +0300

    Time: 0 ms (100.00%) | Memory: 41.9 MB (16.80%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return dfs(root, false);
+    }
+    
+    private int dfs(TreeNode node, boolean isLeft) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null) return isLeft ? node.val : 0;
+        return dfs(node.left, true) + dfs(node.right, false);
+    }
+}
