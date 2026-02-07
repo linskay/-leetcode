62b20038106fa2dde51a8534e0a5d886c0ad0e0e
Alina <168856236+linskay@users.noreply.github.com>
2026-02-07 23:21:55 +0300

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.11%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,42 @@
+public class Solution {
+    private int index = 0;
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return constructBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private TreeNode constructBST(int[] preorder, int lower, int upper) {
+        if (index == preorder.length) {
+            return null;
+        }
+
+        int val = preorder[index];
+        if (val < lower || val > upper) {
+            return null;
+        }
+
+        index++;
+        TreeNode root = new TreeNode(val);
+        root.left = constructBST(preorder, lower, val);
+        root.right = constructBST(preorder, val, upper);
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
